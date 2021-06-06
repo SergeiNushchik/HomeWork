9603556ff5d70e551a11756c2135c2613abb0610
SergeiNushchik <biz.nushchik@gmail.com>
2021-06-06 12:16:18 +0300

    6

--- HomeWork_5/DataContainer.java
+++ HomeWork_5/DataContainer.java
@@ -1,20 +1,35 @@
 package HomeWork_5;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class DataContainer<T> {
 
     private T[] data;
 
-
+    /**
+     * Метод получает из DataContainer'а, из поля data, объект с определенным индексом.
+     * @param index индекс искомого объекта
+     * @return  объект сохраненный под искомым индексом
+     */
     public T getData(int index) {
         return data[index];
     }
 
+    /**
+     * Метод возвращает поле data
+     * @return
+     */
     public T[] getItems() {
         return this.data;
 
     }
+
+    /**
+     * метод производит удаление элемент из нашего поля data по индексу.
+     * @param index
+     * @return
+     */
     boolean delete(int index){
         if (index != data.length-1){
             for (int i = index; i < data.length-1; i++) {
@@ -25,12 +40,19 @@ public class DataContainer<T> {
         return true;
     }
 
-
+    /**
+     * Конструктор
+     * @param data
+     */
      DataContainer(T[] data) {
         this.data = data;
     }
 
-
+    /**
+     * Метод добавляет данные во внутреннее поле data и возвращает номер позиции в которую были вставлены данные, начиная с 0.
+     * @param item полученные данные
+     * @return номер поиции в которую были вставлены данные
+     */
     public int add(T item) {
         int index = 0;
         boolean all = true;
@@ -39,7 +61,6 @@ public class DataContainer<T> {
             data = Arrays.copyOf(data,data.length+1);
             index = data.length-1;
         }
-
         for (int i = 0; i <data.length ; i++) {
             if (data[i] == null) {
                 data[i] = item;
@@ -50,18 +71,31 @@ public class DataContainer<T> {
         }
         return index;
     }
+
+    /**
+     *Метод производит удаление элементов из поля дата
+     * @param item удаляемый элемент
+     * @return  реультат операции
+     */
     boolean delete(T item){
+        boolean result = false;
         for (int i = 0; i < data.length; i++) {
-            if (item.equals(data[i])){
-                data[i] = null;
-             //    for (int j = i; j < data.length; j++) {
-             //       data[j] = data[j+1];
-             //   }
-            } else {
-                return false;
-            }
-        }
-       return true;
+                if ( item.equals(data[i])){
+                    data[i] = null;
+
+                    for (int i1 = i; i1 < data.length-1; i1++) {
+
+                        data[i1] = data[i1+1];
+                        data[i1+1] = null;
+                        result =  true;
+                    }  data = Arrays.copyOf(data,data.length-1);
+                 }
+                }
+        return result;
     }
 
+    @Override
+    public String toString() {
+        return   Arrays.toString(data);
+    }
 }
--- HomeWork_5/HomeWork_5_Main.java
+++ HomeWork_5/HomeWork_5_Main.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.SortedMap;
 
 public class HomeWork_5_Main {
     public static void main(String[] args) {
@@ -13,25 +14,12 @@ public class HomeWork_5_Main {
         int index2 = container.add("Как дела");
         int index3 = container.add("Работаю");
         int index4 = container.add("Давай потом");
-       // String a = container.getData(3);
-        container.delete("Как дела");
-        String text1 = container.getData(index1);
-        String text2 = container.getData(index2);
-        String text3 = container.getData(index3);
-        String text4 = container.getData(index4);
-     // System.out.println(text1); //Привет
-     // System.out.println(text2); //Как дела
-     // System.out.println(text3); //Работаю
-     // System.out.println(text4); //Давай потом
-     // container.delete(index1);
-      // System.out.println(container.getData(index1)); //Как дела
-        container.delete("Как дела");
-        //System.out.println(a);
-        System.out.println(text1); //Привет
-        System.out.println(text2); //Как дела
-        System.out.println(text3); //Работаю
-        System.out.println(text4); //Давай потом
 
+        System.out.println(container.toString());
+
+        container.delete("Давай потом");
+
+        System.out.println(container.toString());
 
     }
 }
